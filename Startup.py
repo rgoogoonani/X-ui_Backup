e7acd9243c2f7c0ed3b06a3265be3e9d82bd17c4
Arash Forootan <arash.forootan1379@gmail.com>
2023-02-09 23:54:18 +0330

    safty an smoothing in startup.py

--- Startup.py
+++ Startup.py
@@ -1,8 +1,13 @@
 import os
 
+botToken = input("Bot Token :")
+while len(botToken)<74 or not botToken.__contains__("bot"):
+    botToken = input("invalid bot token try again: ")
+print(botToken)
+#-------------------------------------------------------------------
 chid=input("Chatid : ")
-if chid=="":
-    chid="189875244"
+while chid=="":
+    chid = input("Chatid : ")
 print(chid)
 #-------------------------------------------------------------------
 Name=input("Server Name : ")
@@ -12,16 +17,13 @@ FileAddres=input("File Addres : ")
 if FileAddres=="":
     FileAddres="/etc/x-ui-english/x-ui-english.db"
 print(FileAddres)
-f=open("/XuiBackUp/config.txt","w")
-
-f.writelines(chid+'\n'+Name+'\n'+FileAddres)
-f.close()
+with open("/XuiBackUp/config.txt","w") as f:
+    f.writelines(f"{chid}\n{Name}\n{FileAddres}\n{botToken}")
 
 
-f=open("/etc/systemd/system/x-uiAutoBackup.service","w")
-f.writelines("[Unit]\nDescription=x-ui Auto Backup\n\n[Service]\nExecStart=/usr/bin/python3 /XuiBackUp/AutoBackup.py\n\n[Install]\nWantedBy=multi-user.target")
+with open("/etc/systemd/system/x-uiAutoBackup.service","w") as f:
+    f.writelines("[Unit]\nDescription=x-ui Auto Backup\n\n[Service]\nExecStart=/usr/bin/python3 /XuiBackUp/AutoBackup.py\n\n[Install]\nWantedBy=multi-user.target")
 #f.writelines("[Unit]\nDescription=x-ui Auto Backup\n\n[Service]\nExecStart=screen /usr/bin/python3 "+str(os.path.abspath(__file__))+"\n\n[Install]\nWantedBy=multi-user.target")
-f.close()
 
 os.system("sudo systemctl daemon-reload")
 os.system("sudo systemctl start x-uiAutoBackup")

5503d23ceaa6219af5b4d5f3683e30898112fba3
Arash Forootan <arash.forootan1379@gmail.com>
2023-02-09 23:59:11 +0330

    safty update in autobackup.py

--- AutoBackup.py
+++ AutoBackup.py
@@ -17,13 +17,14 @@ chid=lines[0]
 Name=lines[1]
 FileAddres=lines[2]
 spl1=FileAddres.split("/")
+token = lines[3]
 t=len(spl1)-1
 FileName=spl1[t]
 
 #-------------------------------------------------------------------
 def upload():
     
-    requests.get("https://api.telegram.org/bot5778651204:AAHWTVjFvM2UqbwWsqzDLr1RsfBH-GC9pV0/sendDocument?chat_id="+chid+"&caption=Server Name : "+Name,files={'document': (FileName, open(FileAddres, 'rb'))})
+    requests.get(f"{token}/sendDocument?chat_id={chid}&caption=Server Name : {Name}",files={'document': (FileName, open(FileAddres, 'rb'))})
     print("uploaded")
 while True:
     upload()

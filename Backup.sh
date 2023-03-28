8e6440850c8d472c77681a0dab30c4cf00a82b13
rgoogoonani <68652911+rgoogoonani@users.noreply.github.com>
2023-03-28 12:46:44 +0330

    Delete Backup.sh

--- Backup.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install python3.9 -y
-sudo apt install python3-pip -y
-sudo mkdir /XuiBackUp
-cd /XuiBackUp
-sudo wget https://raw.githubusercontent.com/rgoogoonani/X-ui_Backup/main/Startup.py
-sudo wget https://raw.githubusercontent.com/rgoogoonani/X-ui_Backup/main/AutoBackup.py
-sudo python3 /XuiBackUp/Startup.py
